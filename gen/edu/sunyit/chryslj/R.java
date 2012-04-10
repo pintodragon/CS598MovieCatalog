de5da2f62e08e4a2cf24eb34f606f81c433b22b6
Justin Chrysler <pintodragon@gmail.com>
2012-04-09 22:08:45 -0400

    Added more to the movie list view.

--- gen/edu/sunyit/chryslj/R.java
+++ gen/edu/sunyit/chryslj/R.java
@@ -26,17 +26,18 @@ public final class R {
         public static final int button1=0x7f060009;
         public static final int button2=0x7f06000a;
         public static final int button3=0x7f06000b;
-        public static final int movie_add_camera=0x7f060010;
-        public static final int movie_add_manual=0x7f06000f;
-        public static final int movie_data_table_layout=0x7f06000e;
-        public static final int movie_list_empty_textview=0x7f060011;
+        public static final int movie_add_camera=0x7f060012;
+        public static final int movie_add_manual=0x7f060011;
+        public static final int movie_data_table_layout=0x7f06000f;
+        public static final int movie_list_empty_textview=0x7f060010;
+        public static final int movie_list_sort_spinner=0x7f06000d;
         public static final int movie_list_table_container=0x7f06000c;
-        public static final int movie_main_table_layout=0x7f06000d;
+        public static final int movie_main_table_layout=0x7f06000e;
         public static final int overlay_view=0x7f060005;
         public static final int preview_running=0x7f060001;
         public static final int preview_surface=0x7f060004;
         public static final int preview_taken=0x7f060003;
-        public static final int quit=0x7f060012;
+        public static final int quit=0x7f060013;
         public static final int take_preview=0x7f060002;
         public static final int textView1=0x7f060007;
     }
@@ -58,12 +59,13 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f070000;
         public static final int main_title=0x7f070002;
-        public static final int movie_add_camera=0x7f07000b;
-        public static final int movie_add_manual=0x7f07000a;
-        public static final int movie_empty_text=0x7f07000c;
-        public static final int movie_table_category=0x7f070008;
-        public static final int movie_table_sort=0x7f070009;
-        public static final int movie_table_title=0x7f070007;
+        public static final int movie_add_camera=0x7f07000c;
+        public static final int movie_add_manual=0x7f07000b;
+        public static final int movie_empty_text=0x7f07000d;
+        public static final int movie_table_index=0x7f070007;
+        public static final int movie_table_personal_rating=0x7f070009;
+        public static final int movie_table_sort=0x7f07000a;
+        public static final int movie_table_title=0x7f070008;
         public static final int quit=0x7f070001;
         public static final int take_pic=0x7f070003;
         public static final int ycrcb_image_data=0x7f070004;
--- src/edu/sunyit/chryslj/ui/MovieListActivity.java
+++ src/edu/sunyit/chryslj/ui/MovieListActivity.java
@@ -3,7 +3,11 @@ package edu.sunyit.chryslj.ui;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -14,10 +18,12 @@ import edu.sunyit.chryslj.movie.MovieManagementSystem;
 public class MovieListActivity extends Activity
 {
     private static final String TAG = MovieListActivity.class.getName();
-    private MovieManagementSystem movieMangementSystem;
+    private MovieManagementSystem movieManagementSystem;
 
     private TableLayout headerTableLayout;
     private TableLayout bodyTableLayout;
+
+    private boolean isSorted = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -25,10 +31,10 @@ public class MovieListActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.movie_list);
 
-        movieMangementSystem = new MovieManagementSystem(this);
-        movieMangementSystem.open();
+        // Connect to the movie management system.
+        movieManagementSystem = new MovieManagementSystem(this);
+        movieManagementSystem.open();
 
-        List<Movie> values = movieMangementSystem.getAllMovies();
         headerTableLayout =
                 (TableLayout) findViewById(R.id.movie_main_table_layout);
         String sortedBy =
@@ -38,19 +44,128 @@ public class MovieListActivity extends Activity
                 .setText(sortedBy);
         bodyTableLayout =
                 (TableLayout) findViewById(R.id.movie_data_table_layout);
+
+        updateTableLayouts();
     }
 
     @Override
     protected void onResume()
     {
-        movieMangementSystem.open();
+        movieManagementSystem.open();
         super.onResume();
     }
 
     @Override
     protected void onPause()
     {
-        movieMangementSystem.close();
+        movieManagementSystem.close();
         super.onPause();
+    }
+
+    private void updateTableLayouts()
+    {
+        List<Movie> movieList = movieManagementSystem.getAllMovies();
+
+        if (movieList.isEmpty())
+        {
+            findViewById(R.id.movie_list_table_container).setVisibility(
+                    View.GONE);
+            bodyTableLayout.removeAllViews();
+            findViewById(R.id.movie_list_empty_textview).setVisibility(
+                    View.VISIBLE);
+        }
+        else
+        {
+            findViewById(R.id.movie_list_empty_textview).setVisibility(
+                    View.GONE);
+            findViewById(R.id.movie_list_table_container).setVisibility(
+                    View.VISIBLE);
+            bodyTableLayout.removeAllViews();
+
+            for (int movieIndex = 0; movieIndex < movieList.size(); movieIndex++)
+            {
+                Movie currentMovie = movieList.get(movieIndex);
+                TableRow newTableRow =
+                        new TableRow(bodyTableLayout.getContext());
+                newTableRow.addView(
+                        createIndexView(movieIndex + 1,
+                                bodyTableLayout.getContext()), 0);
+                newTableRow.addView(
+                        createTitleView(currentMovie.getTitle(),
+                                bodyTableLayout.getContext()), 1);
+                newTableRow.addView(
+                        createCategoryView(currentMovie.getPersonalRaiting(),
+                                bodyTableLayout.getContext()), 2);
+
+                if (isSorted)
+                {
+                    // TODO Figure out the sorted thing.
+                    newTableRow.addView(
+                            createSortedView(currentMovie.getTitle(),
+                                    bodyTableLayout.getContext()), 3);
+                }
+                else
+                {
+                    bodyTableLayout.setColumnCollapsed(3, true);
+                }
+
+                bodyTableLayout.addView(newTableRow);
+            }
+
+            // Force a redraw.
+            bodyTableLayout.invalidate();
+        }
+    }
+
+    private TextView createIndexView(int index, Context context)
+    {
+        TextView textView = new TextView(context);
+        LayoutParams params = textView.getLayoutParams();
+        params.width =
+                (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5,
+                        getResources().getDisplayMetrics());
+        textView.setLayoutParams(params);
+        textView.setText(index);
+
+        return textView;
+    }
+
+    private TextView createTitleView(String title, Context context)
+    {
+        TextView textView = new TextView(context);
+        LayoutParams params = textView.getLayoutParams();
+        params.width =
+                (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                        60, getResources().getDisplayMetrics());
+        textView.setLayoutParams(params);
+        textView.setText(title);
+
+        return textView;
+    }
+
+    private TextView createCategoryView(int personalRating, Context context)
+    {
+        TextView indexView = new TextView(context);
+        LayoutParams params = indexView.getLayoutParams();
+        params.width =
+                (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                        40, getResources().getDisplayMetrics());
+        indexView.setLayoutParams(params);
+        indexView.setText(personalRating);
+
+        return indexView;
+    }
+
+    private TextView createSortedView(String sorted, Context context)
+    {
+        TextView textView = new TextView(context);
+        LayoutParams params = textView.getLayoutParams();
+        params.width =
+                (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                        55, getResources().getDisplayMetrics());
+        textView.setLayoutParams(params);
+        textView.setText(sorted);
+
+        return textView;
     }
 }
